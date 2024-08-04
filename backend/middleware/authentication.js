fe5597119a503aa4a8da9530f900cd2e72420b1d
Amaan7733 <syedamaan7733@gmail.com>
2024-08-04 19:11:33 +0530

    ese hi

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -2,13 +2,24 @@ const CustomError = require("../errors");
 const { isTokenValid } = require("../utils");
 
 const authenticateUser = async (req, res, next) => {
-  const token = req.signedCookies.token;
+  // console.log(req.body);
+  // console.log(req.signedCookies.jwtoken);
+  const {
+    body: { token },
+  } = req.body;
+  // const token = req.body.token;
+  // console.log(token);
+
   if (!token) {
+    console.log("no token");
+
     throw new CustomError.UnauthenticatedError("Invalid Authentication");
   }
   try {
     const { name, userId, role } = isTokenValid({ token });
     req.user = { name, userId, role };
+    console.log(req.user);
+
     next();
   } catch (error) {
     throw new CustomError.UnauthenticatedError("Invalid Authentication");
@@ -17,7 +28,11 @@ const authenticateUser = async (req, res, next) => {
 
 const authorizePermission = (...roles) => {
   return (req, res, next) => {
+    console.log(req.user.role);
+
     if (!roles.includes(req.user.role)) {
+      // console.log("check error");
+
       throw new CustomError.UnauthorizedError(
         "Unauthorized to access this routes"
       );
--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -16,15 +16,11 @@ const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 const attach_ResTOCookie = ({ res, user }) => {
   const token = createJWT({ payload: user });
   const oneDay = 1000 * 60 * 60 * 24;
-  console.log(token);
-
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-    secure: true /*process.env.NODE_ENV === "production"*/,
-    signed: true,
-    sameSite: "None",
-  });
+  // console.log(token);
+  // res.cookie('jwt-auth-token', token, {
+  //   httpOnly: true,
+  //   secure:
+  // })
 };
 
 module.exports = {
