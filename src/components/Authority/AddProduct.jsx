7395a9809ca102c7c693f3031c575daa1809b521
iftekhar1405 <iftekharahmedxyz@gmail.com>
2024-08-24 11:37:51 +0530

    color name not erasing completelty problem solved

--- src/components/Authority/AddProduct.jsx
+++ src/components/Authority/AddProduct.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './AddProduct.css';
-import ProductGridAuth from './ProductGridAuth';
 
 function AddProduct() {
     const [product, setProduct] = useState({
         images: ['', ''],
         brand: '',
         article: '',
-        material: '', // Added material field
-        colors: {},
+        material: '', 
+        colors: [],
         itemSet: [{ size: '', lengths: '' }],
         description: '',
         gender: '',
@@ -23,7 +22,7 @@ function AddProduct() {
         setProduct({ ...product, [name]: value });
     };
 
-    // Handle change for default images
+    // Handle default images
     const handleImageChange = (index, e) => {
         const newImages = [...product.images];
         newImages[index] = e.target.value;
@@ -38,32 +37,40 @@ function AddProduct() {
     };
 
     // Handle color name change
-    const handleColorNameChange = (colorName, newColorName) => {
-        const updatedColors = { ...product.colors };
-        if (newColorName && newColorName !== colorName) {
-            updatedColors[newColorName] = updatedColors[colorName] || [];
-            delete updatedColors[colorName];
-        }
+    const handleColorNameChange = (id, newColorName) => {
+        const updatedColors = product.colors.map(color => 
+            color.id === id ? { ...color, name: newColorName } : color
+        );
         setProduct({ ...product, colors: updatedColors });
     };
 
     // Handle image URL change for a specific color
-    const handleColorImageChange = (colorName, imageIndex, imageUrl) => {
-        const updatedColors = { ...product.colors };
-        updatedColors[colorName][imageIndex] = imageUrl;
+    const handleColorImageChange = (id, imageIndex, imageUrl) => {
+        const updatedColors = product.colors.map(color =>
+            color.id === id ? {
+                ...color,
+                images: color.images.map((img, idx) => idx === imageIndex ? imageUrl : img)
+            } : color
+        );
         setProduct({ ...product, colors: updatedColors });
     };
 
     // Handle adding a new image URL input for a specific color
-    const handleAddColorImage = (colorName) => {
-        const updatedColors = { ...product.colors };
-        updatedColors[colorName] = [...(updatedColors[colorName] || []), ''];
+    const handleAddColorImage = (id) => {
+        const updatedColors = product.colors.map(color =>
+            color.id === id ? { ...color, images: [...color.images, ''] } : color
+        );
         setProduct({ ...product, colors: updatedColors });
     };
 
     // Handle adding a new color with its images
     const handleAddColor = () => {
-        setProduct({ ...product, colors: { ...product.colors, '': [''] } });
+        const newColor = {
+            id: Date.now(),
+            name: '',
+            images: ['']
+        };
+        setProduct({ ...product, colors: [...product.colors, newColor] });
     };
 
     // Handle size and length changes (aligned with schema)
@@ -81,10 +88,8 @@ function AddProduct() {
     const handleSubmit = async (e) => {
         e.preventDefault();
     
-        // Ensure colors object has no empty keys
-        const colors = Object.fromEntries(
-            Object.entries(product.colors).filter(([key, value]) => key && value.length > 0)
-        );
+        // Filter out colors with empty names or images
+        const colors = product.colors.filter(color => color.name && color.images.length > 0);
     
         const productData = {
             ...product,
@@ -92,15 +97,12 @@ function AddProduct() {
         };
     
         try {
-
             const token = (localStorage.getItem('token')); // Retrieve the token from local storage
-    
             const response = await axios.post('https://saleem-footwear-api.vercel.app/api/v1/products', productData, {
                 headers: {
                     Authorization: `Bearer ${token}`, // Include the token in the request headers
                 },
             });
-    
 
             console.log('Product added:', response.data);
             setProduct({
@@ -108,7 +110,7 @@ function AddProduct() {
                 brand: '',
                 article: '',
                 material: '', // Reset material field
-                colors: {},
+                colors: [],
                 itemSet: [{ size: '', lengths: '' }],
                 description: '',
                 gender: '',
@@ -123,107 +125,104 @@ function AddProduct() {
     };
     
     return (
-        <>
-            <div className='add-product'>
-                <h2>Add Product</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className='form-group'>
-                        <label>Default Images:</label>
-                        {product.images.map((imageUrl, index) => (
-                            <div key={index}>
-                                <input
-                                    type='text'
-                                    value={imageUrl}
-                                    onChange={(e) => handleImageChange(index, e)}
-                                />
-                            </div>
-                        ))}
-                        <button type='button' onClick={handleAddImage}>Add More Default Images</button>
-                    </div>
-                    <div className='form-group'>
-                        <label>Brand:</label>
-                        <input type='text' name='brand' value={product.brand} onChange={handleChange} />
-                    </div>
-                    <div className='form-group'>
-                        <label>Article:</label>
-                        <input type='text' name='article' value={product.article} onChange={handleChange} />
-                    </div>
-                    <div className='form-group'>
-                        <label>Material:</label> {/* Added input field for material */}
-                        <input type='text' name='material' value={product.material} onChange={handleChange} />
-                    </div>
-                    <div className='form-group'>
-                        <label>Colors and Images:</label>
-                        {Object.entries(product.colors).map(([colorName, images], colorIndex) => (
-                            <div key={colorIndex}>
-                                <input
-                                    type='text'
-                                    placeholder='Color Name'
-                                    value={colorName}
-                                    onChange={(e) => handleColorNameChange(colorName, e.target.value)}
-                                />
-                                {images.map((imageUrl, imageIndex) => (
-                                    <div key={imageIndex}>
-                                        <input
-                                            type='text'
-                                            placeholder='Image URL'
-                                            value={imageUrl}
-                                            onChange={(e) => handleColorImageChange(colorName, imageIndex, e.target.value)}
-                                        />
-                                    </div>
-                                ))}
-                                <button type='button' onClick={() => handleAddColorImage(colorName)}>Add More Images</button>
-                            </div>
-                        ))}
-                        <button type='button' onClick={handleAddColor}>Add More Colors</button>
-                    </div>
-                    <div className='form-group'>
-                        <label>Item Sets:</label>
-                        {product.itemSet.map((set, index) => (
-                            <div key={index} className='size-length'>
-                                <input
-                                    type='text'
-                                    placeholder='Size'
-                                    value={set.size}
-                                    onChange={(e) => handleSetChange(index, 'size', e)}
-                                />
-                                <input
-                                    type='text'
-                                    placeholder='Pcs'
-                                    value={set.lengths}
-                                    onChange={(e) => handleSetChange(index, 'lengths', e)}
-                                />
-                            </div>
-                        ))}
-                        <button type='button' onClick={handleAddSet}>Add More Item Sets</button>
-                    </div>
-                    <div className='form-group'>
-                        <label>Description:</label>
-                        <textarea name='description' value={product.description} onChange={handleChange}></textarea>
-                    </div>
-                    <div className='form-group'>
-                        <label>Gender:</label>
-                        <select name='gender' value={product.gender} onChange={handleChange}>
-                            <option value=''>Select Gender</option>
-                            <option value='male'>Male</option>
-                            <option value='female'>Female</option>
-                            <option value='unisex'>Unisex</option>
-                            <option value='kids'>Kids</option>
-                        </select>
-                    </div>
-                    <div className='form-group'>
-                        <label>Price:</label>
-                        <input type='number' name='price' value={product.price} onChange={handleChange} />
-                    </div>
-                    <div className='form-group'>
-                        <label>Category:</label>
-                        <input type='text' name='category' value={product.category} onChange={handleChange} />
-                    </div>
-                    <button type='submit'>Add Product</button>
-                </form>
-            </div>
-            
-        </>
+        <div className='add-product'>
+            <h2>Add Product</h2>
+            <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <label>Default Images:</label>
+                    {product.images.map((imageUrl, index) => (
+                        <div key={index}>
+                            <input
+                                type='text'
+                                value={imageUrl}
+                                onChange={(e) => handleImageChange(index, e)}
+                            />
+                        </div>
+                    ))}
+                    <button type='button' onClick={handleAddImage}>Add More Default Images</button>
+                </div>
+                <div className='form-group'>
+                    <label>Brand:</label>
+                    <input type='text' name='brand' value={product.brand} onChange={handleChange} />
+                </div>
+                <div className='form-group'>
+                    <label>Article:</label>
+                    <input type='text' name='article' value={product.article} onChange={handleChange} />
+                </div>
+                <div className='form-group'>
+                    <label>Material:</label>
+                    <input type='text' name='material' value={product.material} onChange={handleChange} />
+                </div>
+                <div className='form-group'>
+                    <label>Colors and Images:</label>
+                    {product.colors.map((color, colorIndex) => (
+                        <div key={color.id}>
+                            <input
+                                type='text'
+                                placeholder='Color Name'
+                                value={color.name}
+                                onChange={(e) => handleColorNameChange(color.id, e.target.value)}
+                            />
+                            {color.images.map((imageUrl, imageIndex) => (
+                                <div key={imageIndex}>
+                                    <input
+                                        type='text'
+                                        placeholder='Image URL'
+                                        value={imageUrl}
+                                        onChange={(e) => handleColorImageChange(color.id, imageIndex, e.target.value)}
+                                    />
+                                </div>
+                            ))}
+                            <button type='button' onClick={() => handleAddColorImage(color.id)}>Add More Images</button>
+                        </div>
+                    ))}
+                    <button type='button' onClick={handleAddColor}>Add More Colors</button>
+                </div>
+                <div className='form-group'>
+                    <label>Item Sets:</label>
+                    {product.itemSet.map((set, index) => (
+                        <div key={index} className='size-length'>
+                            <input
+                                type='text'
+                                placeholder='Size'
+                                value={set.size}
+                                onChange={(e) => handleSetChange(index, 'size', e)}
+                            />
+                            <input
+                                type='text'
+                                placeholder='Pcs'
+                                value={set.lengths}
+                                onChange={(e) => handleSetChange(index, 'lengths', e)}
+                            />
+                        </div>
+                    ))}
+                    <button type='button' onClick={handleAddSet}>Add More Item Sets</button>
+                </div>
+                <div className='form-group'>
+                    <label>Description:</label>
+                    <textarea name='description' value={product.description} onChange={handleChange}></textarea>
+                </div>
+                <div className='form-group'>
+                    <label>Gender:</label>
+                    <select name='gender' value={product.gender} onChange={handleChange}>
+                        <option value=''>Select Gender</option>
+                        <option value='male'>Male</option>
+                        <option value='female'>Female</option>
+                        <option value='unisex'>Unisex</option>
+                        <option value='kids'>Kids</option>
+                    </select>
+                </div>
+                <div className='form-group'>
+                    <label>Price:</label>
+                    <input type='number' name='price' value={product.price} onChange={handleChange} />
+                </div>
+                <div className='form-group'>
+                    <label>Category:</label>
+                    <input type='text' name='category' value={product.category} onChange={handleChange} />
+                </div>
+                <button type='submit'>Add Product</button>
+            </form>
+        </div>
     );
 }
 
