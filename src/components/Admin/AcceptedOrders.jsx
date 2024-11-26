e955359f0031d1abea39fa395a0ab7c241ededeb
iftekhar1405 <iftekharahmedxyz@gmail.com>
2024-11-26 10:26:45 +0530

    print functionality added

--- src/components/Admin/AcceptedOrders.jsx
+++ src/components/Admin/AcceptedOrders.jsx
@@ -1,30 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import {
   Box,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
   Button,
-  Spinner,
-  Text,
   Divider,
-} from '@chakra-ui/react';
+  HStack,
+  IconButton,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { BiPrinter } from "react-icons/bi";
+import { useReactToPrint } from "react-to-print";
 
 const URL = "https://saleem-footwear-api.vercel.app/api/v1";
 
 function AcceptedOrders() {
   const [acceptedOrders, setAcceptedOrders] = useState([]);
   const [loading, setLoading] = useState(false);
+  const printRef = useRef(null);
+  const handlePrint = useReactToPrint({
+    contentRef: printRef,
+    onBeforeGetContent: () => {
+      const element = document.querySelectorAll(".overflow");
+      const originalOverflow = element.style.overflowX;
+
+      element.style.overflowX = "visible";
+      return () => {
+        element.style.overflow = originalOverflow;
+      };
+    },
+  });
 
   useEffect(() => {
     const fetchAcceptedOrders = async () => {
       try {
         setLoading(true);
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem("token");
         const response = await axios.get(`${URL}/order`, {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -32,15 +49,17 @@ function AcceptedOrders() {
         });
 
         if (response.status >= 200 && response.status < 300) {
-          const filteredOrders = response.data.data.filter(order => order.status === 'accepted');
+          const filteredOrders = response.data.data.filter(
+            (order) => order.status === "accepted"
+          );
           setAcceptedOrders(filteredOrders);
         } else {
-          alert('Unexpected response status: ' + response.status);
+          alert("Unexpected response status: " + response.status);
         }
-        
+
         setLoading(false);
       } catch (error) {
-        alert('Error fetching accepted orders: ' + error.message);
+        alert("Error fetching accepted orders: " + error.message);
         setLoading(false);
       }
     };
@@ -50,24 +69,28 @@ function AcceptedOrders() {
 
   const updateOrderStatus = async (orderId, status) => {
     try {
-      const token = localStorage.getItem('token');
-      const response = await axios.patch(`${URL}/order/status/${orderId}`, 
-        { status }, 
+      const token = localStorage.getItem("token");
+      const response = await axios.patch(
+        `${URL}/order/status/${orderId}`,
+        { status },
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
-        });
+        }
+      );
 
       if (response.status >= 200 && response.status < 300) {
-        alert('Order status updated successfully');
-        
-        if (status === 'rejected') {
-          setAcceptedOrders(prevOrders => prevOrders.filter(order => order._id !== orderId));
+        alert("Order status updated successfully");
+
+        if (status === "rejected") {
+          setAcceptedOrders((prevOrders) =>
+            prevOrders.filter((order) => order._id !== orderId)
+          );
         }
       } else {
-        alert('Unexpected response status: ' + response.status);
+        alert("Unexpected response status: " + response.status);
       }
     } catch (error) {
       alert(`Error updating order status to ${status}: ` + error.message);
@@ -80,19 +103,37 @@ function AcceptedOrders() {
 
   return (
     <Box p={4}>
-      <Text fontSize="2xl" mb={4}>Accepted Orders</Text>
+      <Text fontSize="2xl" mb={4}>
+        Accepted Orders
+      </Text>
       {acceptedOrders.length === 0 ? (
         <Text>No accepted orders</Text>
       ) : (
-        acceptedOrders.map(order => (
-          <Box key={order._id} mb={8}>
-            <Text fontSize="xl" fontWeight="bold">Order ID: {order._id}<br/>
-            Customer : {order.userId.name}</Text>
-            <Box overflowX="auto"> {/* Enable horizontal scrolling */}
+        acceptedOrders.map((order) => (
+          <Box key={order._id} mb={8} ref={printRef}>
+            <HStack justifyContent={"space-between"}>
+              <Text fontSize="xl" fontWeight="bold">
+                Order ID: {order._id}
+                <br />
+                Customer : {order.userId.name}
+              </Text>
+              <IconButton icon={<BiPrinter />} onClick={handlePrint} />
+            </HStack>
+            <Box overflowX="auto" className="overflow">
+              {" "}
+              {/* Enable horizontal scrolling */}
               <Table variant="striped" mt={4}>
                 <Thead>
                   <Tr>
-                    <Th>Product ID</Th>
+                    <Th
+                      sx={{
+                        "@media print": {
+                          display: "none",
+                        },
+                      }}
+                    >
+                      Product ID
+                    </Th>
                     <Th>Article</Th>
                     <Th>Brand</Th>
                     <Th>Price</Th>
@@ -102,24 +143,48 @@ function AcceptedOrders() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {order.items.map(item => (
+                  {order.items.map((item) => (
                     <Tr key={item.productId._id}>
-                      <Td>{item.productId._id}</Td>
-                      <Td>{item.productId.article || 'N/A'}</Td>
-                      <Td>{item.productId.brand || 'N/A'}</Td>
+                      <Td
+                        sx={{
+                          "@media print": {
+                            display: "none",
+                          },
+                        }}
+                      >
+                        {item.productId._id}
+                      </Td>
+                      <Td>{item.productId.article || "N/A"}</Td>
+                      <Td>{item.productId.brand || "N/A"}</Td>
                       <Td>₹{item.price}</Td>
                       <Td>{item.color}</Td>
-                      <Td>{item.itemSet && item.itemSet.length > 0
-                        ? item.itemSet.map(i => `${i.size} (Pcs: ${i.lengths})`).join(', ')
-                        : "N/A"}</Td>
+                      <Td>
+                        {item.itemSet && item.itemSet.length > 0
+                          ? item.itemSet
+                              .map((i) => `${i.size} (Pcs: ${i.lengths})`)
+                              .join(", ")
+                          : "N/A"}
+                      </Td>
                       <Td>{item.quantity}</Td>
                     </Tr>
                   ))}
                 </Tbody>
               </Table>
             </Box>
-            <Box mt={4}>
-              <Button colorScheme="red" onClick={() => updateOrderStatus(order._id, 'rejected')}>Reject Order</Button>
+            <Box
+              mt={4}
+              sx={{
+                "@media print": {
+                  display: "none",
+                },
+              }}
+            >
+              <Button
+                colorScheme="red"
+                onClick={() => updateOrderStatus(order._id, "rejected")}
+              >
+                Reject Order
+              </Button>
             </Box>
             <Divider my={4} />
           </Box>
--- src/components/Admin/RejectedOrders.jsx
+++ src/components/Admin/RejectedOrders.jsx
@@ -1,30 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import {
   Box,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
   Button,
-  Spinner,
-  Text,
   Divider,
-} from '@chakra-ui/react';
+  HStack,
+  IconButton,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { BiPrinter } from "react-icons/bi";
+import { useReactToPrint } from "react-to-print";
 
 const URL = "https://saleem-footwear-api.vercel.app/api/v1";
 
 function RejectedOrders() {
   const [rejectedOrders, setRejectedOrders] = useState([]);
   const [loading, setLoading] = useState(false);
+  const printRef = useRef(null);
+  const handlePrint = useReactToPrint({
+    contentRef: printRef,
+    onBeforeGetContent: () => {
+      const element = document.querySelectorAll(".overflow");
+      const originalOverflow = element.style.overflowX;
+
+      element.style.overflowX = "visible";
+      return () => {
+        element.style.overflow = originalOverflow;
+      };
+    },
+  });
 
   useEffect(() => {
     const fetchRejectedOrders = async () => {
       try {
         setLoading(true);
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem("token");
         const response = await axios.get(`${URL}/order`, {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -32,15 +49,17 @@ function RejectedOrders() {
         });
 
         if (response.status >= 200 && response.status < 300) {
-          const filteredOrders = response.data.data.filter(order => order.status === 'rejected');
+          const filteredOrders = response.data.data.filter(
+            (order) => order.status === "rejected"
+          );
           setRejectedOrders(filteredOrders);
         } else {
-          alert('Unexpected response status: ' + response.status);
+          alert("Unexpected response status: " + response.status);
         }
-        
+
         setLoading(false);
       } catch (error) {
-        alert('Error fetching rejected orders: ' + error.message);
+        alert("Error fetching rejected orders: " + error.message);
         setLoading(false);
       }
     };
@@ -50,56 +69,62 @@ function RejectedOrders() {
 
   const handleReinstateOrder = async (orderId) => {
     try {
-      const token = localStorage.getItem('token');
-      const response = await axios.patch(`${URL}/order/status/${orderId}`, 
-      { status: 'pending' }, 
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      
+      const token = localStorage.getItem("token");
+      const response = await axios.patch(
+        `${URL}/order/status/${orderId}`,
+        { status: "pending" },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
       if (response.status >= 200 && response.status < 300) {
-        alert('Order status updated to pending');
-        setRejectedOrders(prevOrders => prevOrders.filter(order => order._id !== orderId));
+        alert("Order status updated to pending");
+        setRejectedOrders((prevOrders) =>
+          prevOrders.filter((order) => order._id !== orderId)
+        );
       } else {
-        alert('Unexpected response status: ' + response.status);
+        alert("Unexpected response status: " + response.status);
       }
     } catch (error) {
-      alert('Error updating order status to pending: ' + error.message);
+      alert("Error updating order status to pending: " + error.message);
     }
   };
 
   const handleReinstateItem = async (orderId, itemId) => {
     try {
-      const token = localStorage.getItem('token');
-      const response = await axios.patch(`${URL}/order/item/${orderId}/status`, 
+      const token = localStorage.getItem("token");
+      const response = await axios.patch(
+        `${URL}/order/item/${orderId}/status`,
         {
           itemId,
-          status: 'pending'
+          status: "pending",
         },
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
-        });
+        }
+      );
 
       if (response.status >= 200 && response.status < 300) {
-        const updatedOrders = rejectedOrders.map(order => {
+        const updatedOrders = rejectedOrders.map((order) => {
           if (order._id === orderId) {
             return {
               ...order,
-              items: order.items.filter(item => item._id !== itemId)
+              items: order.items.filter((item) => item._id !== itemId),
             };
           }
           return order;
         });
         setRejectedOrders(updatedOrders);
-        alert('Item reinstated successfully');
+        alert("Item reinstated successfully");
       } else {
-        alert('Unexpected response status: ' + response.status);
+        alert("Unexpected response status: " + response.status);
       }
     } catch (error) {
       alert(`Error reinstating item: ${error.message}`);
@@ -112,45 +137,88 @@ function RejectedOrders() {
 
   return (
     <Box p={4}>
-      <Text fontSize="2xl" mb={4}>Rejected Orders</Text>
+      <Text fontSize="2xl" mb={4}>
+        Rejected Orders
+      </Text>
       {rejectedOrders.length === 0 ? (
         <Text>No rejected orders</Text>
       ) : (
-        rejectedOrders.map(order => (
-          <Box key={order._id} mb={8}>
-            <Text fontSize="xl" fontWeight="bold">Order ID: {order._id}<br/>
-            Customer : {order.userId.name}</Text>
-            <Box overflowX="auto">
+        rejectedOrders.map((order) => (
+          <Box key={order._id} mb={8} ref={printRef}>
+            <HStack justifyContent={"space-between"}>
+              <Text fontSize="xl" fontWeight="bold">
+                Order ID: {order._id}
+                <br />
+                Customer : {order.userId.name}
+              </Text>
+              <IconButton icon={<BiPrinter />} onClick={handlePrint} />
+            </HStack>
+            <Box overflowX="auto" className="overflow">
               <Table variant="simple" mt={4}>
                 <Thead>
                   <Tr>
-                    <Th>Product ID</Th>
+                    <Th
+                      sx={{
+                        "@media print": {
+                          display: "none",
+                        },
+                      }}
+                    >
+                      Product ID
+                    </Th>
                     <Th>Article</Th>
                     <Th>Brand</Th>
                     <Th>Price</Th>
                     <Th>Color</Th>
                     <Th>Item Set</Th>
                     <Th>Quantity</Th>
-                    <Th>Actions</Th>
+                    <Th
+                      sx={{
+                        "@media print": {
+                          display: "none",
+                        },
+                      }}
+                    >
+                      Actions
+                    </Th>
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {order.items.map(item => (
+                  {order.items.map((item) => (
                     <Tr key={item._id}>
-                      <Td>{item.productId._id}</Td>
+                      <Td
+                        sx={{
+                          "@media print": {
+                            display: "none",
+                          },
+                        }}
+                      >
+                        {item.productId._id}
+                      </Td>
                       <Td>{item.productId.article}</Td>
                       <Td>{item.productId.brand}</Td>
                       <Td>₹{item.price}</Td>
                       <Td>{item.color}</Td>
-                      <Td>{item.itemSet && item.itemSet.length > 0
-                        ? item.itemSet.map(i => `${i.size} (Pcs: ${i.lengths})`).join(', ')
-                        : "N/A"}</Td>
+                      <Td>
+                        {item.itemSet && item.itemSet.length > 0
+                          ? item.itemSet
+                              .map((i) => `${i.size} (Pcs: ${i.lengths})`)
+                              .join(", ")
+                          : "N/A"}
+                      </Td>
                       <Td>{item.quantity}</Td>
                       <Td>
-                        <Button 
-                          colorScheme="blue" 
+                        <Button
+                          colorScheme="blue"
                           size="sm"
-                          onClick={() => handleReinstateItem(order._id, item._id)}
+                          onClick={() =>
+                            handleReinstateItem(order._id, item._id)
+                          }
+                          sx={{
+                            "@media print": {
+                              display: "none",
+                            },
+                          }}
                         >
                           Reinstate Item
                         </Button>
@@ -160,20 +228,17 @@ function RejectedOrders() {
                 </Tbody>
               </Table>
             </Box>
-            
-            {/* Customer Info */}
-            <Box mt={4} p={4} borderWidth="1px" borderRadius="lg">
-              <Text fontSize="lg" fontWeight="bold">Customer Information</Text>
-              <Text>User ID: {order.userId?._id || 'N/A'}</Text>
-              <Text>Name: {order.userId?.name || 'N/A'}</Text>
-              <Text>Address: {order.userId?.address || 'N/A'}</Text>
-            </Box>
 
             {/* Order Actions */}
             <Box mt={4}>
-              <Button 
-                colorScheme="blue" 
+              <Button
+                colorScheme="blue"
                 onClick={() => handleReinstateOrder(order._id)}
+                sx={{
+                  "@media print": {
+                    display: "none",
+                  },
+                }}
               >
                 Reinstate Entire Order
               </Button>
